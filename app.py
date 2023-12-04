0db7975793fd052339a58d47e728a848b117721f
german.settino <german.settino@menta.global>
2023-12-04 20:37:14 -0300

    wip

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask_cors import CORS
 
 import mysql.connector
 
+from werkzeug.utils import secure_filename # ESTO ES PARA QUE EL NOMBRE DE LA IMAGEN NO TENGA CARACTERES RAROS
+
+import os
+import time
+
 app = Flask(__name__)
 CORS(app) #ESTO HABILITA CORS PARA TODAS LAS RUTAS
 
@@ -20,28 +25,35 @@ class Catalogo:
         self.cursor.execute(f"USE `{database}`")
         
             
-    def agregar_producto(self, nombre, precio, stock):
-        sql = "INSERT INTO `db-tpf`.`productos` (`nombre`, `precio`, `stock`) VALUES ('" + nombre + "', " + precio + ", " + stock + ");"
-    
-        self.cursor.execute(sql)
+    def agregar_producto(self, nombre, precio, stock, imagen):
+        #sql = "INSERT INTO `db-tpf`.`productos` (`nombre`, `precio`, `stock`) VALUES ('" + nombre + "', " + precio + ", " + stock + ");"
+        sql = "INSERT INTO `db-tpf`.`productos` (`nombre`, `precio`, `stock`, `imagen_url` ) VALUES (%s, %s, %s, %s);" # ESTO ES UNA CONSULTA PARAMETRIZADA (ES MAS SEGURA) %s ES UNA VARIABLE QUE SE VA A CAMBIAR POR OTRO VALOR EVITA EL SQL INJECTION
+        valores = (nombre, precio, stock, imagen)
+        
+        self.cursor.execute(sql, valores)
         self.conn.commit()
         return True
             
     def eliminar_producto(self, codigo):
-        sql = "DELETE FROM `db-tpf`.`productos` WHERE (`id` = " + str(codigo) + ");"
+        #sql = "DELETE FROM `db-tpf`.`productos` WHERE (`id` = " + str(codigo) + ");"
+        sql = (f"DELETE FROM `db-tpf`.`productos` WHERE (`id` = {codigo});") 
         self.cursor.execute(sql)
         self.conn.commit()
         return True
         
     def traer_producto_por_id(self, codigo):
-        sql = "SELECT * FROM `db-tpf`.`productos` WHERE (`id` = " + str(codigo) + ");"
+        #sql = "SELECT * FROM `db-tpf`.`productos` WHERE (`id` = " + str(codigo) + ");"
+        sql = (f"SELECT * FROM `db-tpf`.`productos` WHERE (`id` = {codigo});")
         self.cursor.execute(sql)
         producto = self.cursor.fetchone()
         return producto
         
     def modificar_producto(self, codigo, nombre, precio, stock):
-        sql = "UPDATE `db-tpf`.`productos` SET `nombre` = '" + nombre + "', `precio` = " + precio + ", `stock` = " + stock + " WHERE (`id` = " + str(codigo) + ");"
-        self.cursor.execute(sql)
+        #sql = "UPDATE `db-tpf`.`productos` SET `nombre` = '" + nombre + "', `precio` = " + precio + ", `stock` = " + stock + " WHERE (`id` = " + str(codigo) + ");"
+        sql = "UPDATE `db-tpf`.`productos` SET `nombre` = %s, `precio` = %s, `stock` = %s WHERE (`id` = %s);"
+        valores = (nombre, precio, stock, codigo)
+        
+        self.cursor.execute(sql, valores)
         self.conn.commit()
         return True
         
@@ -56,7 +68,11 @@ class Catalogo:
 ####################################################
 # PROGRAMA PRINCIPAL
 
-catalogo = Catalogo(host='localhost', user='root', password='', database='db-tpf')
+# CREAMOS UN OBJETO DE LA CLASE CATALOGO QUE LE PASA LOS PARAMETROS DE CONEXION
+catalogo = Catalogo(host='localhost', user='root', password='', database='db-tpf') 
+
+
+RUTA_DESTINO = "./static/imagenes/"
 
 @app.route("/productos", methods=["POST"]) #ESTO ES UN DECORADOR
 def agregar_producto():
@@ -64,9 +80,17 @@ def agregar_producto():
     nombre = request.form['nombre']
     precio = request.form['precio']
     stock = request.form['stock']
+    imagen = request.files['imagen'] # ESTO ES UNA VARIABLE DE TIPO FILE STORAGE
     
-    si_se_agrego = catalogo.agregar_producto(nombre, precio, stock)
+    nombre_imagen = secure_filename(imagen.filename) # ESTO ES PARA QUE EL NOMBRE DE LA IMAGEN NO TENGA CARACTERES RAROS
+    nombre_base, extension = os.path.splitext(nombre_imagen) # ESTO SEPARA EL NOMBRE DE LA IMAGEN DE LA EXTENSION
+    imagen_url = f"{nombre_base}{extension}" # ESTO ES PARA QUE EL NOMBRE DE LA IMAGEN SEA UNICO
+    
+    # ESTO ES PARA GUARDAR LA IMAGEN EN UNA CARPETA    
+    si_se_agrego = catalogo.agregar_producto(nombre, precio, stock, imagen_url)
     if si_se_agrego:
+        imagen.save(os.path.join(RUTA_DESTINO, imagen_url))
+        
         return jsonify({"mensaje": "producto agregado"}), 200 # ESTO ES UNA RESPUESTA HTTP OK
     else:
        return jsonify({"mensaje": "Error"}), 400 # ESTO ES UNA RESPUESTA HTTP ERROR
@@ -74,7 +98,20 @@ def agregar_producto():
 @app.route("/productos", methods=["GET"])
 def traer_productos():
     productos = catalogo.traer_productos()
-    return jsonify(productos), 200 # ESTO ES UNA RESPUESTA HTTP OK
+    
+    productos_json = [] # ESTO ES UNA LISTA DE DICCIONARIOS
+    
+    for producto in productos:
+        producto_json = { # ESTO ES UN DICCIONARIO
+            "id": producto[0],
+            "nombre": producto[1],
+            "precio": producto[2],
+            "stock": producto[3],
+            "imagen_url": producto[4]
+        }
+        productos_json.append(producto_json)
+        
+    return jsonify(productos_json), 200 # ESTO ES UNA RESPUESTA HTTP OK
 
 @app.route("/productos/<int:codigo>", methods=["DELETE"])
 def eliminar_producto(codigo):
@@ -87,7 +124,17 @@ def eliminar_producto(codigo):
 @app.route("/productos/<int:codigo>", methods=["GET"])
 def traer_producto_por_id(codigo):
     producto = catalogo.traer_producto_por_id(codigo)
-    return jsonify(producto), 200
+    if producto:
+        producto_json = { # ESTO ES UN DICCIONARIO
+            "id": producto[0],
+            "nombre": producto[1],
+            "precio": producto[2],
+            "stock": producto[3],
+            "imagen_url": producto[4]
+        }
+        return jsonify(producto_json), 200
+    else:
+        return jsonify({"mensaje": "Producto no encontrado"}), 400
 
 @app.route("/productos/<int:codigo>", methods=["PUT"])
 def modificar_producto(codigo):    
